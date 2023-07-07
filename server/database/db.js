3a3f809b6ee8a9ec7b712af5f9ee252932a6d167
aashapun11 <106722144+aashapun11@users.noreply.github.com>
2023-07-07 12:02:03 +0530

    understandGit

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const connectDB = async() =>{
     try{
         // Mongodb connection string
-        const con = await mongoose.connect(process.env.MONGO_CONNECTION_URL,{
+        const con = await mongoose.connect("mongodb+srv://aashamgr111:tarapun@cluster0.zq1pm69.mongodb.net/?retryWrites=true&w=majority",{
         
         })
         console.log('MongoDB connected: ${con.connection.host}');
